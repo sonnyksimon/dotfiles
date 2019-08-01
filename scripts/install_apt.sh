f80c28ece2918d9361250e421428aed966629b5d
Sonny Kothapally <me@sonnyksimon.com>
2019-08-01 02:43:12 -0400

    add packages

--- scripts/install_apt.sh
+++ scripts/install_apt.sh
@@ -5,8 +5,12 @@ APTITUDE_OPTIONS=${APTITUDE_OPTIONS:-"-y"}
 apt-get update
 
 cat <<PACKAGES | xargs apt-get install $APTITUDE_OPTIONS
+make
 git
 vim
+screen
+tmux
+ranger
 python3
 python3-pip
 python3-venv
--- scripts/install_yum.sh
+++ scripts/install_yum.sh
@@ -5,5 +5,14 @@ YUM_OPTIONS=${YUM_OPTIONS:-"-y"}
 yum update
 
 cat <<PACKAGES | xargs yum install $YUM_OPTIONS
+make
 git
+vim
+screen
+tmux
+ranger
+curl
+httpie
+epel-release
+jq
 PACKAGES
