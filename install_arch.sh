24be0d7328bc074294660af9a479c43bdde6190c
Sonny Kothapally <me@sonnyksimon.com>
2019-08-01 07:26:05 -0400

    install for arch

---
+++ install_arch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+    echo "ERROR: Must be run with root privileges."
+    exit 1
+fi
+
+RUNDIR=$(dirname $0)
+SCRIPTDIR="$RUNDIR/scripts"
+
+function important() {
+    echo -e "\033[31m${1}\033[0m"
+}
+
+echo
+important "This script assumes that you're in your own box and you have root privileges"
+important "for installs. It also assumes that you're using archlinux--you gotta know alleast"
+important "that mane."
+echo
+important "Before proceeding, if this is your first time installing, stop here and read"
+important "the script (scripts/install_pacman.sh). It's got pretty much all the information"
+important "to prevent common issues."
+echo
+important "NEVER RUN THIS ON A MACHINE OTHER THAN YOURS, IT WILL DELETE ALL YOUR FILES,"
+important "AND OTHER WEIRD HAIRY STUFF. ONLY USE FOR FRESH INSTALLATIONS OF ARCHLINUX."
+read -er -n1 -p "proceed [Y/n]" response
+if [[ $response =~ ^[Yy]$ || $response == "" ]]; then
+    echo "Excellent. Here we go!"
+    $SCRIPTDIR/install_pacman.sh
+fi
---
+++ scripts/install_pacman.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PACMAN_OPTIONS=${PACMAN_OPTIONS:-"--noconfirm"}
+
+pacman -Syu
+
+cat <<PACKAGES | xargs pacman -S $PACMAN_OPTIONS
+make
+git
+vim
+screen
+tmux
+highlight
+ranger
+net-tools
+python
+python-pip
+ruby
+libpqxx
+curl
+httpie
+jq
+PACKAGES
