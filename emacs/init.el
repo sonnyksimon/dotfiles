b0b42949cdd4471bc3abbc15669a0325fa8ead91
Sonny Kothapally <me@sonnyksimon.com>
2023-10-29 13:16:35 -0400

    emacs: toggle-show-trailing-whitespace

--- emacs/init.el
+++ emacs/init.el
@@ -1,2 +1,9 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/dracula")
 (load-theme 'dracula t)
+
+(setq-default show-trailing-whitespace t)
+
+(defun toggle-show-trailing-whitespace ()
+  "Toggle `show-trailing-whitespace'."
+  (interactive)
+  (setq show-trailing-whitespace (not show-trailing-whitespace)))
