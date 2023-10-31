10a98ed637e7715bff1e061675b9bf7237f662ba
Sonny Kothapally <me@sonnyksimon.com>
2023-10-31 17:55:19 -0400

    emacs: keep backups together

--- emacs/init.el
+++ emacs/init.el
@@ -7,3 +7,11 @@
   "Toggle `show-trailing-whitespace'."
   (interactive)
   (setq show-trailing-whitespace (not show-trailing-whitespace)))
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
